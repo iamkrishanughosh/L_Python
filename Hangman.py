e43ec429e7be6057cd7b6eaac2114372fcb6c639
Krishanu Ghosh <imkrishanughosh@gmail.com>
2022-01-23 17:12:53 +0530

    Hangman Algorithm Complete

--- Hangman.py
+++ Hangman.py
@@ -4,6 +4,13 @@ word_list = ["elephant", "mouse", "turtle", "dog"]
 word = random.choice(word_list).lower()
 
 
+def l2w(list2word):
+    w = ""
+    for char in list2word:
+        w += char
+    return w
+
+
 def create_blanks():
     blank = []
     for blnk in range(1, len(word) + 1):
@@ -19,7 +26,7 @@ life = 0
 # print to test
 print(lives)
 # for life in range(0, lives):
-while life < lives:
+while life < lives and l2w(blanks) != word:
     guess = input("Guess a letter: ").lower()
     if guess in word:
         for position in range(len(word)):
@@ -27,11 +34,12 @@ while life < lives:
             if letter == guess:
                 blanks[position] = letter
                 print(blanks)
+                check2win = l2w(blanks)
+                if check2win == word:
+                    print("You Win !!!")
     else:
         life += 1
         if life == lives:
             print("Wrong guess!!! \nGame Over !!! No lives left.")
         else:
             print(f"Wrong guess !!! {lives - life} lives left.")
-
-
