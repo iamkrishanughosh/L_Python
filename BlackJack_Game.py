608c071a6e32cc99be6996a322f7cecf9b89a684
Krishanu Ghosh <imkrishanughosh@gmail.com>
2022-02-10 19:58:42 +0530

    BlackJack Game betting system

--- BlackJack_Game.py
+++ BlackJack_Game.py
@@ -1,14 +1,17 @@
 import random
 
 user_credits = 500
+user_wants_to_play = True
 
 
+# Function to deal a single card at a time.
 def deal_cards():
     cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
     card = random.choice(cards)
     return card
 
 
+# Function to calculate score for one instance.
 def calculate_score(cards):
     if sum(cards) == 21 and len(cards) == 2:
         return 0
@@ -18,32 +21,63 @@ def calculate_score(cards):
     return sum(cards)
 
 
+# Function to compare scores to declare the winner.
+# def compare_scores(user, computer):
+#     if user > 21 and computer > 21:
+#         return "You went over. You lose 😤"
+#
+#     if user == computer:
+#         return "Draw 🙃"
+#     elif computer == 0:
+#         return "Lose, opponent has Blackjack 😱"
+#     elif user == 0:
+#         return "You win with a Blackjack 😎"
+#     elif user > 21:
+#         return "You went over. You lose 😭"
+#     elif computer > 21:
+#         return "Opponent went over. You win 😁"
+#     elif user > computer:
+#         return "You win 😃"
+#     else:
+#         return "You lose 😤"
+
+
 def compare_scores(user, computer):
     if user > 21 and computer > 21:
-        return "You went over. You lose 😤"
+        print("You went over. You lose 😤")
+        return "L"
 
     if user == computer:
-        return "Draw 🙃"
+        print("Draw 🙃")
+        return "T"
     elif computer == 0:
-        return "Lose, opponent has Blackjack 😱"
+        print("Lose, opponent has Blackjack 😱")
+        return "L"
     elif user == 0:
-        return "You win with a Blackjack 😎"
+        print("You win with a Blackjack 😎")
+        return "W"
     elif user > 21:
-        return "You went over. You lose 😭"
+        print("You went over. You lose 😭")
+        return "L"
     elif computer > 21:
-        return "Opponent went over. You win 😁"
+        print("Opponent went over. You win 😁")
+        return "W"
     elif user > computer:
-        return "You win 😃"
+        print("You win 😃")
+        return "W"
     else:
-        return "You lose 😤"
+        print("You lose 😤")
+        return "L"
 
 
+# Function to print final hands of both players. Don't mind me I'm laaaazy.
 def show_final_scores(p1_cards, p1_score, p2_cards, p2_score):
     print("\n")
     print(f"Your final hand : {p1_cards} . Your Score {p1_score}")
     print(f"Computer's final hand : {p2_cards} . Computer Score {p2_score}")
 
 
+# Function defines the game logic for a proper BlackJack game.
 def game_logic():
     user_cards = []
     computer_cards = []
@@ -65,7 +99,7 @@ def game_logic():
             is_game_over = True
             show_final_scores(user_cards, user_score, computer_cards, computer_score)
             print(compare_scores(user_score, computer_score))
-            quit()
+            break
         else:
             user_want_card = input("Do you want to draw another card ? (Y/N) : ")
             if user_want_card in ["Y", "y"]:
@@ -80,7 +114,43 @@ def game_logic():
         print(f"Computer's first card : {computer_cards} . Computer's score {computer_score}")
 
     show_final_scores(user_cards, user_score, computer_cards, computer_score)
-    print(compare_scores(user_score, computer_score))
+    win_loss = compare_scores(user_score, computer_score)
+    return win_loss
 
 
-game_logic()
+while user_credits > 0 and user_wants_to_play:
+    print(f"You currently have {user_credits} coins.")
+    bet = int(input("Place your bet : "))
+    user_credits -= bet
+    win_loss_4_bet = game_logic()
+    if win_loss_4_bet in ["W", "w"]:
+        user_credits = user_credits + (bet * 2)
+        print(f"Yay !!! You now have {user_credits} coins.")
+        user_play = input("Press Y to play again or Press N to cash-out : ")
+        if user_play in ["Y","y"]:
+            user_wants_to_play = True
+        else:
+            print(f"You cashed out {user_credits} coins.")
+            user_wants_to_play = False
+    elif win_loss_4_bet in ["T", "t"]:
+        user_credits = user_credits + bet
+        print(f"Thats a draw. You still have {user_credits} coins.")
+        user_play = input("Press Y to play again or Press N to cash-out : ")
+        if user_play in ["Y", "y"]:
+            user_wants_to_play = True
+        else:
+            print(f"You cashed out {user_credits} coins.")
+            user_wants_to_play = False
+    else:
+        user_credits = user_credits
+        if user_credits > 0:
+            print(f"Oh noooo !!! You now have {user_credits} coins left.")
+            user_play = input("Press Y to play again or Press N to cash-out : ")
+            if user_play in ["Y", "y"]:
+                user_wants_to_play = True
+            else:
+                print(f"You cashed out {user_credits} coins.")
+                user_wants_to_play = False
+        else:
+            print(f"Oh Nooo !!! You ran out of coins.")
+            user_wants_to_play = False
